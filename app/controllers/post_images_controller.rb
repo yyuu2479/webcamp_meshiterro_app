0b4074f2f218d726c92a6d0d81e9f8fc0b297eea
yyuu2479 <yyuu2479@gmail.com>
2021-08-01 13:05:10 +0000

    meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,9 +11,11 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
   
   def destroy
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '44767e1fa8797ceec7478a76cc6c5aeb461a10f9046b3728d6e27dbf121d8cf1496c0441444dd71e5318be77a51d313bef753d21bd1fff6e3e58f9966f24a4e5'
