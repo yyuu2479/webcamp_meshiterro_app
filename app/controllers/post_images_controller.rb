e1766a80e76a1c85b6788f7d26a7deab9f24542c
yyuu2479 <yyuu2479@gmail.com>
2021-08-01 10:59:48 +0000

    meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,8 +1,13 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
   end
   
   def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -14,4 +19,12 @@ class PostImagesController < ApplicationController
   def destroy
     
   end
+  
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:image, :shop_name, :caption)
+  end
+  
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,5 @@
 class PostImage < ApplicationRecord
   belongs_to :user
+  attachment :image
   
 end
