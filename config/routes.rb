161784d701fdb81331d5975e4d9abe489151d187
yyuu2479 <yyuu2479@gmail.com>
2021-08-01 08:49:18 +0000

    meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   devise_for :users
-  
   root to: 'homes#top'
+  resources :post_images, only:[:new, :create, :index, :show, :destroy]
 end
