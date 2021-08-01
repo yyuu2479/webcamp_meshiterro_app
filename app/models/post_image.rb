0cbc88bb0113afdfabb563d80358d2a8f7f4479a
yyuu2479 <yyuu2479@gmail.com>
2021-08-01 08:46:29 +0000

    meshiterro

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,4 @@
 class PostImage < ApplicationRecord
-  belongs_to
+  belongs_to :user
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
   devise_for :users
   
   root to: 'homes#top'
