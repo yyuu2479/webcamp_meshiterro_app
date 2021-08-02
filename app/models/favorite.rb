908680ca5708fef43b5f897d9c639c0f046adf64
yyuu2479 <yyuu2479@gmail.com>
2021-08-02 12:59:40 +0000

    meshiterro

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post_image
   
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,8 +4,8 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  def favorite_by?(user)
-    favorites.where(user_id: user.id).exsts?
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
   end
 
 end
