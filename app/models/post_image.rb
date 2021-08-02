627636b311f38d071402ef7b7d3baecad7b9feb9
yyuu2479 <yyuu2479@gmail.com>
2021-08-02 11:27:25 +0000

    meshiterro

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,5 +2,10 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def favorite_by?(user)
+    favorites.where(user_id: user.id).exsts?
+  end
 
 end
