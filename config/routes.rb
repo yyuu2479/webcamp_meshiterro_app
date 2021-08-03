041d244df08dfd9a3c930da71d14f6bc766016a4
yyuu2479 <yyuu2479@gmail.com>
2021-08-03 04:29:10 +0000

    user-edit-create

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :post_comments, only:[:create, :destroy]
   end
   
-  resources :users, only:[:show, :edit]
+  resources :users, only:[:show, :edit, :update]
 end
